f4af4efb64e9f0ca53fea8a33a147e5262a6bbc0
409-08-Natthanan <54171@yupparaj.ac.th>
2026-08-29 14:38:24 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -8,10 +8,6 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
-if "ans3_val" not in st.session_state:
-    st.session_state.ans3_val = ""
-if "ans4_val" not in st.session_state:
-    st.session_state.ans4_val = ""
 
 
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
@@ -47,24 +43,11 @@ def show_result_dialog(ans1, ans2):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-    # ตรวจข้อ 3
-    if u_ans3 == "Avocado":
-        st.success("✅ ข้อ 3: ถูกต้อง")
-        score += 1
-    else:
-        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
-
-    # ตรวจข้อ 4
-    if u_ans4 == "Glasses":
-        st.success("✅ ข้อ 4: ถูกต้อง")
-        score += 1
-    else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
-      
+    # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 4:
+    if score == 2:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
@@ -96,20 +79,12 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-ans3 = st.text_input(
-    "ข้อ 2: Cats love to eat `A _ _ _ _ d o`. 🥑",
-    value=st.session_state.ans3_val,
-)
-ans4 = st.text_input(
-    "ข้อ 2: Cats love to eat `_ _ _ _ s e s`. 👓",
-    value=st.session_state.ans4_val,
-)
 
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
+
+# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 
 
 # 4. ปุ่มส่งคำตอบ
@@ -123,7 +98,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2)
 
 st.divider()
 st.write("นางสาวณัฐนันท์ บุญพรหม เลขที่ 8 ม.4/9")
