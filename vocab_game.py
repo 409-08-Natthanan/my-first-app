3c78a87b269b027966f568e85027d5a057ee0770
409-08-Natthanan <54171@yupparaj.ac.th>
2026-08-29 15:14:41 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,7 +52,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
      # ตรวจข้อ 3
-    if u_ans3 == "Avocado":
+    if u_ans3 == "watermelon":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
@@ -100,7 +100,7 @@ ans2 = st.text_input(
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: `A _ _ _ _ d o` are green fruits and have a bland taste. 🥑",
+    "ข้อ 3: I don't really like `w a _ _ _ m e l o n` because it has a lot of seeds. 🍉",
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
